9ef42aa1bd5d3ea065323e7d2ba891f96d3173f1
Joe Chung <joechung-msft@users.noreply.github.com>
2025-08-24 15:50:59 -0400

    Disable @eslint/js

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,4 @@
+import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -9,6 +10,7 @@ export default tseslint.config([
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
+      js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
