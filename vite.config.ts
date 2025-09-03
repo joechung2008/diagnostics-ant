ddfe7e5500e871d487282119bbfb56466cdb4648
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-03 00:00:51 -0400

    Use manualChunks to split Ant Design into smaller chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,32 @@ import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes("node_modules/antd/es/config-provider") ||
+            id.includes("node_modules/antd/es/theme")
+          ) {
+            return "antd_config_provider_theme";
+          } else if (id.includes("node_modules/antd/es/flex")) {
+            return "antd_flex";
+          } else if (id.includes("node_modules/antd/es/menu")) {
+            return "antd_menu";
+          } else if (id.includes("node_modules/antd/es/table")) {
+            return "antd_table";
+          } else if (id.includes("node_modules/antd/es/tabs")) {
+            return "antd_tabs";
+          } else if (id.includes("node_modules/antd/es/typography")) {
+            return "antd_typography";
+          } else {
+            return null;
+          }
+        },
+      },
+    },
+  },
   plugins: [react()],
   test: {
     coverage: {
