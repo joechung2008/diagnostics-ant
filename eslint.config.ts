d165ceed0bdcdf059b536a36c64bee1e6c5e3ee1
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-03 20:57:05 -0400

    Add React and React DOM ESLint plugins

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,9 @@
 import prettierConfig from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
+import reactDomPlugin from "eslint-plugin-react-dom";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactPlugin from "eslint-plugin-react-x";
 import { globalIgnores } from "eslint/config";
 import globals from "globals";
 import { fileURLToPath } from "node:url";
@@ -10,6 +12,8 @@ import tseslint from "typescript-eslint";
 export default [
   globalIgnores(["dist", "coverage", "src/**/*.d.ts", "*.config.ts"]),
   ...tseslint.configs.recommended,
+  reactPlugin.configs["recommended-typescript"],
+  reactDomPlugin.configs.recommended,
   reactHooks.configs["recommended-latest"],
   reactRefresh.configs.vite,
   prettierConfig,
